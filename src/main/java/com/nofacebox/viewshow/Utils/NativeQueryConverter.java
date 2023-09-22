6087ae65ce3d7ae0bfa45d534732e1d7bdb0de3c
chenchenghung <pihsia.lian@gmail.com>
2023-09-22 17:39:41 +0800

    add query converter

---
+++ src/main/java/com/nofacebox/viewshow/Utils/NativeQueryConverter.java
@@ -0,0 +1,30 @@
+package com.nofacebox.viewshow.Utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class NativeQueryConverter {
+    private final static ObjectMapper objectMapper = new ObjectMapper();
+    public NativeQueryConverter(){
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public static <T> List<T> convert(List<Map<String, Object>> dataList, Class<T> objClass) {
+        List<T> res = new ArrayList<>();
+        for (Map<String, Object> item: dataList) {
+            try {
+                String json = objectMapper.writeValueAsString(item);
+                T bean = objectMapper.readValue(json, objClass);
+                res.add(bean);
+            } catch (JsonProcessingException e){
+                e.printStackTrace();
+            }
+        }
+        return res;
+    }
+}
